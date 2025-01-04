1eb136af79416cea37b4f5c5c544feeef4f6ec09
Ashifji <160580273+ashifji@users.noreply.github.com>
2025-01-04 14:06:17 +0530

    second commit

--- Testing/Program.cs
+++ Testing/Program.cs
@@ -2,12 +2,34 @@
 
 namespace Testing
 {
-    class Program
+    abstract class hello
     {
+        public abstract void student();
+
+    }
+
+    class Program:hello
+    {
+
+        public override void student()
+        {
+            Console.WriteLine("this is abstract method with abstract class");
+
+        }
+        
+
         static void Main(string[] args)
         {
             Console.WriteLine("hello world!");
+            Console.WriteLine("==========================");
+            Program pg = new Program();
+            Console.WriteLine("Abstract method initialized");
+            pg.student();
+
+            
             Console.ReadLine();
+
+
         }
     }
 }
